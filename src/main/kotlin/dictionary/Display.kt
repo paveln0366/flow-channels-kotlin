1fe7206993426a91c29ab327166ee18852aa43bf
Pavel Potapov <potapov@goozix.com>
2026-04-29 20:49:28 +0300

    Add reactive style

--- src/main/kotlin/dictionary/Display.kt
+++ src/main/kotlin/dictionary/Display.kt
@@ -2,28 +2,36 @@ package org.example.dictionary
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import java.awt.BorderLayout
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
 import javax.swing.*
 
 object Display {
 
+    private lateinit var queries: Flow<String>
+
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     private val repository = Repository
+    private var loadingJob: Job? = null
 
     private val enterWordLabel = JLabel("Enter word: ")
-    private val searchField = JTextField(20)
+    private val searchField = JTextField(20).apply {
+        addKeyListener(object : KeyAdapter() {
+            override fun keyReleased(e: KeyEvent?) {
+                loadDefinitions()
+            }
+        })
+    }
     private val searchButton = JButton("Search").apply {
         addActionListener {
-            scope.launch {
-                isEnabled = false
-                resultArea.text = "Loading..."
-                val word = searchField.text.trim()
-                val definition = repository.loadDefinition(word).joinToString("\n\n")
-                resultArea.text = definition.ifEmpty { "Not found" }
-                isEnabled = true
-            }
+            loadDefinitions()
         }
     }
     private val resultArea = JTextArea(25, 50).apply {
@@ -43,8 +51,29 @@ object Display {
         pack()
     }
 
+    private fun loadDefinitions() {
+
+    }
+
     fun show() {
         mainFrame.isVisible = true
+    }
+
+    init {
+        queries
+            .onEach {
+                searchButton.isEnabled = false
+                resultArea.text = "Loading..."
+            }.map {
+                repository.loadDefinition(it)
+            }
+            .map {
+                it.joinToString("\n\n").ifEmpty { "Not found" }
+            }
+            .onEach {
+                resultArea.text = it
+                searchButton.isEnabled = true
+            }.launchIn(scope)
     }
 }
 
